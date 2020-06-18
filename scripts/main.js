466e730ed26bb9b8ff4a64653f5cc2aeb2b670de
Tony Eneh <aeneh90@gmail.com>
2020-06-18 04:41:31 +0100

    feat: toggle menu visibility with buttons

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/**
+ * toggle flyout menu
+ */
+
+ const hideMenu = (e)=>{
+    const menu = document.querySelector('.main-menu');
+    const closeBtn = e.currentTarget;
+
+    menu.classList.add('hidden-menu');
+    closeBtn.classList.add('hidden-menu');
+ }
+
+ const showMenu = (e)=>{
+    const menu = document.querySelector('.main-menu');
+    const closeBtn = document.querySelector('.close-btn');
+
+    menu.classList.remove('hidden-menu');
+    closeBtn.classList.remove('hidden-menu');
+ }
